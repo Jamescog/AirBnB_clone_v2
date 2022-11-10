d9ab1169f83b5ea3804237b6f5413ae16e03b3fe
Jamescog <jamescog72@gmail.com>
2022-11-10 23:24:28 +0300

    add task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,37 @@
+"""Starts a Flask web application
+    - the web application will be listening on 0.0.0.0, port 80
+
+    Routes:
+            - /: displays "Hello HBNB!"
+            - /hbnb: displays "HNBN"
+            - /c/<text>: displays "C" followed by the value of the text
+"""
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hnbn():
+    """displays 'HBNB' """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is(text):
+    """displays what c is!"""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
