ca120ff3410561009b6867d2576a164bf0bfe2b1
Jamescog <jamescog72@gmail.com>
2022-11-10 22:59:08 +0300

    add task 0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,22 @@
+"""
+Starts the Flask web application
+    - it will listen on 0.0.0.0, port 5000
+    Routes:
+            - /: display "Hello HBNB!"
+"""
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """displays the response for '/' request"""
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
